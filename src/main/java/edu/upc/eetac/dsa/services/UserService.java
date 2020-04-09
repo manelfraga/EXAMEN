6e2740b8d8a65a31e1ede1bd3f78d894fceb64dc
krunal badsiwal <krunalmiracle@gmail.com>
2020-04-09 20:43:39 +0200

    Rest Api & Swagger Implemented but services yet not completed

---
+++ src/main/java/edu/upc/eetac/dsa/services/UserService.java
@@ -0,0 +1,10 @@
+package edu.upc.eetac.dsa.services;
+import io.swagger.annotations.Api;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+public class UserService {
+    //Empty UserService
+}
